28fd171ff0b640bb435c04843654a489305ba41a
Minseok Choe <choems@outlook.com>
2025-05-10 15:40:04 +0900

    chore: run formattor

--- src/app/chat/components/ChatBox.tsx
+++ src/app/chat/components/ChatBox.tsx
@@ -1,36 +1,40 @@
-import React, { useRef, useEffect } from 'react';
-import TypingIndicator from './TypingIndicator';
+import React, { useRef, useEffect } from "react";
+import TypingIndicator from "./TypingIndicator";
 
 interface ChatBoxProps {
-    messages: { sender: string; text: string }[];
-    isTyping: boolean;
-    peerTyping: boolean;
+  messages: { sender: string; text: string }[];
+  isTyping: boolean;
+  peerTyping: boolean;
 }
 
-const ChatBox: React.FC<ChatBoxProps> = ({ messages, isTyping, peerTyping }) => {
-    const messagesEndRef = useRef<HTMLDivElement | null>(null);
+const ChatBox: React.FC<ChatBoxProps> = ({
+  messages,
+  isTyping,
+  peerTyping,
+}) => {
+  const messagesEndRef = useRef<HTMLDivElement | null>(null);
 
-    useEffect(() => {
-        messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
-    }, [messages, isTyping]);
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages, isTyping]);
 
-    return (
-        <div className="chat-box">
-            {messages.map((message, index) => (
-                <div
-                    key={index}
-                    className={`chat-message ${
-                        message.sender === 'user' ? 'chat-right' : 'chat-left'
-                    }`}
-                >
-                    <span>{message.text}</span>
-                </div>
-            ))}
-            {peerTyping && <TypingIndicator position="left" sender="admin" />}
-            {isTyping && <TypingIndicator position="right" sender="user" />}
-            <div ref={messagesEndRef} />
+  return (
+    <div className="chat-box">
+      {messages.map((message, index) => (
+        <div
+          key={index}
+          className={`chat-message ${
+            message.sender === "user" ? "chat-right" : "chat-left"
+          }`}
+        >
+          <span>{message.text}</span>
         </div>
-    );
+      ))}
+      {peerTyping && <TypingIndicator position="left" sender="admin" />}
+      {isTyping && <TypingIndicator position="right" sender="user" />}
+      <div ref={messagesEndRef} />
+    </div>
+  );
 };
 
 export default ChatBox;
